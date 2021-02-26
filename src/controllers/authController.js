9c5961980d70f96002de46619495585702a8466c
DeboraSamara <deborah50play@gmail.com>
2021-02-26 14:47:38 -0300

    Modificações

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.post('/register', async (req, res)=>{
  try{
-     const user = await User.create(req.body)
+     const user = await User.create(req.body);
 
      return res.send({ user});
 
